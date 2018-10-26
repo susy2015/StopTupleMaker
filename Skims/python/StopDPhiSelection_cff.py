e8c6c3747819667b6cedbda13ed71c7f19f15762
Nathaniel <pastika@cern.ch>
2018-10-26 17:37:37 -0500

    Move last necessary file from Skim to SkimAUX and remove Skim directory

--- Skims/python/StopDPhiSelection_cff.py
+++
@@ -1,6 +0,0 @@
-
-from StopTupleMaker.Skims.dPhiFilter_cfi import *
-
-jetsMETDPhiFilter = dPhiFilter.clone()
-jetsMETDPhiFilter.jetSrc = cms.InputTag("patJetsPF")
-jetsMETDPhiFilter.metSrc = cms.InputTag("patMETsPF")
